df1e8dd9b008c8f87b2740e2bc8cb5fbdd7f58eb
Srikar403 <58563346+Srikar403@users.noreply.github.com>
2021-06-30 15:26:08 +0530

    added gorund

---
+++ game.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+screen_width = 675
+screen_height = 732
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Flappy Bird')
+#game - variables
+ground_scroll = 0
+scroll_speed = 4
+
+#loading images
+bg = pygame.image.load('img/bg.png')
+ground_img = pygame.image.load('img/ground.png')
+
+
+
+
+
+run = True
+while run:
+
+    clock.tick(fps)
+
+    screen.blit(bg,(0,0))
+    
+    screen.blit(ground_img,(ground_scroll,600))
+    ground_scroll-=scroll_speed
+    if abs(ground_scroll)>35:
+        ground_scroll=0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.display.update()
+
+pygame.quit()
